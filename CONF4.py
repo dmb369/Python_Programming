fe5b1dd75114af363e00ea4bf9e170a4aa6b04c0
dmb369 <116587999+dmb369@users.noreply.github.com>
2023-02-13 22:23:16 +0530

    Add files via upload

---
+++ CONF4.py
@@ -0,0 +1,28 @@
+r = int(input())
+k = int(input())
+if r > k:
+  print("Ram is heavier than Karan")
+elif k >r:
+  print("Karan is heavier than Ram")
+else:
+  print("Ram & Karan have the same weight!")
+
+r = int(input())
+k = int(input())
+if r > k:
+  print("Ram is heavier than Karan")
+elif k >r:
+  print("Karan is heavier than Ram")
+else:
+  print("Ram & Karan have the same weight!")
+
+r = int(input())
+k = int(input())
+if r > k:
+  print("Ram is heavier than Karan")
+elif k >r:
+  print("Karan is heavier than Ram")
+else:
+  print("Ram & Karan have the same weight!")
+
+
